530d1794e7deb5ecd8cf1760827077e2d62e8319
silvestr_andreevi4 <74321169+kle1gmc@users.noreply.github.com>
2025-05-29 14:01:44 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -20,13 +20,13 @@ class FlaskAppTest(unittest.TestCase):
         self.driver.get("http://127.0.0.1:5000/")  # URL главной страницы
         time.sleep(1)
         title = self.driver.title
-        self.assertIn("Главная", title)  # Проверяем, что на главной странице в заголовке содержится слово "Home"
+        self.assertIn("Главная", title)  # Проверяем, что на главной странице в заголовке содержится слово "Главная"
 
     def test_catalog_page(self):
         self.driver.get("http://127.0.0.1:5000/catalog")  # URL страницы каталога
         time.sleep(1)
         title = self.driver.title
-        self.assertIn("Каталог товаров", title)  # Проверяем, что на странице каталога в заголовке содержится слово "Catalog"
+        self.assertIn("Каталог товаров", title)  # Проверяем, что на странице каталога в заголовке содержится слово "Каталог"
 
         # Дополнительно проверяем, что на странице есть товары, если они есть в базе данных
         products = self.driver.find_elements(By.CSS_SELECTOR, ".product")
@@ -36,7 +36,7 @@ class FlaskAppTest(unittest.TestCase):
         self.driver.get("http://127.0.0.1:5000/about")  # URL страницы "О нас"
         time.sleep(1)
         title = self.driver.title
-        self.assertIn("О нас", title)  # Проверяем, что на странице "О нас" в заголовке содержится слово "About"
+        self.assertIn("О нас", title)  # Проверяем, что на странице "О нас" в заголовке содержится слово "О нас"
 
 
 if __name__ == "__main__":
